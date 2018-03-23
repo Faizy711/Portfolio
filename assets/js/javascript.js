b8aa5ef9e3fe7c907ff0cc4123c929057d37d015
Faizy711 <faizan.s711@gmail.com>
2018-03-22 20:31:14 -0500

    onclicks

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 /* BAFFLE */
 /* Animation speed value can be increased or decreased. Same with duration */
 var b = baffle('.baffle', { characters: '█▓▒░', speed: 150 }).reveal(2500); /* Runs animation only once, upon page load */
-var bl = baffle('.baffle-longer', { characters: '█▓▒░', speed: 150, duration: 3000 }).reveal(3000); /* Animation duration set to 3 seconds */
+var bl = baffle('.baffle-longer', { speed: 150, duration: 3000 }).reveal(3000); /* Animation duration set to 3 seconds */
 var bp = baffle('.baffle-prolonged', { characters:'█▓▒░', speed: 300, duration: 10000 }).reveal(10000); /* Animation duration set to 10 seconds */
 var bf = baffle('.baffle-forever', {characters: '█▓▒░', speed: 150}).start(); /* Animation continues on an endless loop */
 });
